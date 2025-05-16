1f513d8d7dd39a31196f72b4f75485fdbcce4278
kchanakiyan <chanakiyank.cse2024@citchennai.net>
2025-05-16 13:02:13 +0530

    Finalize env vars and connect to Railway

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 // MySQL connection
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
